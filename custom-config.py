fc97cc749790b4ef67638bd54dc71dd93a89f53c
Kavindu404 <66483737+Kavindu404@users.noreply.github.com>
2025-03-05 23:40:48 -0500

    Update custom-config.py

--- custom-config.py
+++ custom-config.py
@@ -1,23 +1,17 @@
 import torch
 
 class CFG:
-    IMG_PATH = './dataset'
-    ANNOTATIONS_PATH = './dataset/annotations/annotations.json'
-    TRAIN_IMAGES_DIR = './dataset/images/train'
-    VAL_IMAGES_DIR = './dataset/images/val'
-    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    # Dataset name
+    # Path settings
     DATASET = "custom_dataset"
+    TRAIN_DATASET_DIR = "./dataset"
+    VAL_DATASET_DIR = "./dataset"
+    TEST_IMAGES_DIR = "./dataset/images/val"
+    TRAIN_ANNOTATIONS_FILE = "./dataset/annotations/640.json"
+    VAL_ANNOTATIONS_FILE = "./dataset/annotations/640.json"
     
-    TRAIN_DDP = False  # Set this to False for single GPU training
-    NUM_WORKERS = 2
-    PIN_MEMORY = True
-    LOAD_MODEL = False
-
-    # You might need to adjust this based on your dataset
-    N_VERTICES = 192  # maximum number of vertices per image in dataset
-    
+    # Model settings
+    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    N_VERTICES = 192  # maximum number of vertices per image in dataset.
     SINKHORN_ITERATIONS = 100
     MAX_LEN = (N_VERTICES*2) + 2
     IMG_SIZE = 224
@@ -30,36 +24,37 @@ class CFG:
     vertex_loss_weight = 1.0
     perm_loss_weight = 10.0
     SHUFFLE_TOKENS = False
-
-    BATCH_SIZE = 16  # Adjust based on your GPU memory
+    
+    # Training settings
+    BATCH_SIZE = 8  # Adjust based on your GPU memory
+    NUM_WORKERS = 2
+    PIN_MEMORY = True
+    LOAD_MODEL = False
     START_EPOCH = 0
-    NUM_EPOCHS = 100  # You can adjust this
+    NUM_EPOCHS = 100
     MILESTONE = 0
     SAVE_BEST = True
     SAVE_LATEST = True
     SAVE_EVERY = 10
     VAL_EVERY = 1
 
+    # Model architecture
     MODEL_NAME = f'vit_small_patch{PATCH_SIZE}_{INPUT_SIZE}_dino'
     NUM_PATCHES = int((INPUT_SIZE // PATCH_SIZE) ** 2)
 
+    # Optimizer settings
     LR = 4e-4
     WEIGHT_DECAY = 1e-4
 
-    generation_steps = (N_VERTICES * 2) + 1
+    # Generation settings
+    generation_steps = (N_VERTICES * 2) + 1  # sequence length during prediction
     run_eval = False
 
-    EXPERIMENT_NAME = f"train_Pix2Poly_{DATASET}_singleGPU_{MODEL_NAME}_initialLR_{LR}_bs_{BATCH_SIZE}_Nv_{N_VERTICES}_Nbins{NUM_BINS}_{NUM_EPOCHS}epochs"
+    # Experiment name
+    EXPERIMENT_NAME = f"train_Pix2Poly_custom_dataset_run1_{MODEL_NAME}_Linear_{vertex_loss_weight}xVertexLoss_{perm_loss_weight}xPermLoss_bs_{BATCH_SIZE}_Nv_{N_VERTICES}_Nbins{NUM_BINS}_{NUM_EPOCHS}epochs"
 
-    if "debug" in EXPERIMENT_NAME:
-        BATCH_SIZE = 10
-        NUM_WORKERS = 0
-        SAVE_BEST = False
-        SAVE_LATEST = False
-        SAVE_EVERY = NUM_EPOCHS
-        VAL_EVERY = 50
-
+    # Checkpoint path (if loading a model)
     if LOAD_MODEL:
-        CHECKPOINT_PATH = f"runs/{EXPERIMENT_NAME}/logs/checkpoints/latest.pth"
+        CHECKPOINT_PATH = f"runs/{EXPERIMENT_NAME}/logs/checkpoints/latest.pth"  # full path to checkpoint to be loaded if LOAD_MODEL=True
     else:
         CHECKPOINT_PATH = ""
